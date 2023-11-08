f6b838ef172b36d6b7ac455af3bd621d00332947
Jakub Kita <jakub.kita44@gmail.com>
2023-11-08 21:30:02 +0100

    use props forwarding to optimize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import Header from "./components/Header/Header.jsx";
-import CoreConcepts from "./components/CoreConcepts.jsx";
-import Examples from "./components/Examples.jsx";
+import Header from './components/Header/Header.jsx'
+import CoreConcepts from './components/CoreConcepts.jsx'
+import Examples from './components/Examples.jsx'
+import Section from './components/Section.jsx'
 
 function App() {
-  return (
-    // nice and clear build with the use of components feature of React
-    <>
-      <Header />
-      <main>
-        <CoreConcepts />
-        <Examples/>
-      </main>
-    </>
-  );
+	return (
+		// nice and clear build with the use of components feature of React
+		<>
+			<Header />
+			<main>
+				<Section title='Core concepts' id='core-concepts'>
+					<CoreConcepts />
+				</Section>
+				<Section id='examples' title='Examples'>
+					<Examples />
+				</Section>
+			</main>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -4,15 +4,11 @@ import CoreConcept from './CoreConcept'
 export default function CoreConcepts() {
 	return (
 		<>
-			{/* Rebuild component! Transfer section to separate component */}
-			<section id='core-concepts'>
-				<h2>Core Concepts</h2>
-				<ul>
-					{CORE_CONCEPTS.map(conceptItem => (
-						<CoreConcept key={conceptItem.title} {...conceptItem} />
-					))}
-				</ul>
-			</section>
+			<ul>
+				{CORE_CONCEPTS.map(conceptItem => (
+					<CoreConcept key={conceptItem.title} {...conceptItem} />
+				))}
+			</ul>
 		</>
 	)
 }
