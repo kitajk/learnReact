de8d8615387a25219327fdbe1941ff9b12c5f505
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-11-18 13:08:20 +0100

    fixed feature

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,24 @@
+import { useState } from 'react';
 import reactImg from '../../assets/react-core-concepts.png';
 import './Header.css';
-
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
+	return Math.floor(Math.random() * (max + 1));
 }
-
 export default function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
+	const [isFeature, setIsFeature] = useState(false);
+	const description = reactDescriptions[genRandomInt(2)];
+	return (
+		<header>
+			<img src={reactImg} alt="Stylized atom" /> <h1>React Essentials</h1>
+			<p>
+				{description} React concepts you will need for almost any app you are
+				going to build!
+			</p>
+			<button onClick={() => setIsFeature((prev) => !prev)}>
+				New Feature :D
+			</button>
+			{isFeature ? <p>Feature Off</p> : <p>Feature On</p>}
+		</header>
+	);
 }
